ecd7492a690f319e35da302c38a70ea7da039605
Dmitry <joydnby@gmail.com>
2019-07-13 17:27:02 +0300

    reorganized server files structure

--- src/server/server.js
+++ src/server/server.js
@@ -1,85 +1,19 @@
-import express from 'express';
-import fetch from 'node-fetch';
-import bodyParser from 'body-parser';
-import cors from 'cors';
-import dotenv from 'dotenv';
 import React from 'react';
 import { renderToString } from 'react-dom/server';
 import { StaticRouter } from 'react-router-dom';
 import App from '../shared/index';
 import NewsBox from '../shared/components/newsbox';
 
-const app = express();
-app.use(cors());
-app.use(express.static('public'));
-app.use(bodyParser.urlencoded({ extended: true }));
 
-const API_KEY = dotenv.config().parsed.API_KEY;
-const host = '0.0.0.0';
-const port = process.env.PORT || 5000;
-let url;
+export function render(initialState, location, value) {
 
-app.listen(port, host, () => console.log(`Listening on http://localhost:${port}/`));
+    const app = initialState === 'app'
+        ? <App />
+        : <StaticRouter location={location}>
+            <NewsBox value={value} />
+        </StaticRouter>
 
-app.get('/', (req, res) => {
-    const markup = renderToString(<App />);
-    res.send(`
-    <!DOCTYPE html>
-    <html>
-      <head>
-       <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-        <link rel="stylesheet" type="text/css" href="/bundle.css">
-        <link href="https://fonts.googleapis.com/css?family=Indie+Flower" rel="stylesheet">
-        <title>news</title>
-      </head>
-      <body>
-        <div id="App">${markup}</div>
-        <script type="text/javascript" src="/bundle.js"></script>
-      </body>
-    </html>
-  `);
-});
+    const content = renderToString(app);
+    return content;
 
-app.get('/:link', (req, res) => {
-    const link = req.params.link;
-    const markup = renderToString(
-        <StaticRouter location={req.url}>
-            <NewsBox value={link} />
-        </StaticRouter>,
-    );
-
-    res.send(`
-        <!DOCTYPE html>
-        <html>
-          <head>
-           <meta charset="utf-8">
-            <meta name="viewport" content="width=device-width, initial-scale=1">
-            <link rel="stylesheet" type="text/css" href="/bundle.css">
-            <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-            <link href="https://fonts.googleapis.com/css?family=Indie+Flower" rel="stylesheet">
-            <script>window.LINK = ${JSON.stringify({ link })}</script>
-            <title>news</title>
-          </head>
-          <body>
-            <div id="App">${markup}</div>
-            <script type="text/javascript" src="/bundle.js"></script>
-          </body>
-        </html>
-  `);
-});
-
-app.post(/.*/, (req, res) => {
-    url = req.originalUrl === '/top'
-        ? `https://newsapi.org/v2/top-headlines?country=us&page=${req.body.page}&pageSize=10&apiKey=${API_KEY}`
-        : `https://newsapi.org/v2/everything?q=${req.originalUrl}&language=en&sortBy=popularity&page=${req.body.page}&pageSize=10&apiKey=${API_KEY}`;
-
-    fetch(url).then((response) => {
-            if (response.ok) {
-                return response.json();
-            }
-            throw new Error('failed');
-        }, networkError => console.log(networkError.message))
-        .then(jsonResponse => res.json(jsonResponse));
-});
+}
