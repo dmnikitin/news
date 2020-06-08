e4a8da730bccd59ec212f9bcf678d0cff3de2ca0
dmitry <joydnby@gmail.com>
2020-06-08 10:14:06 +0300

    fix: heroku case sensitive issues

---
+++ src/shared/components/sourcesBox.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { instances, headers } from '../../data/data';
+
+export default function SourcesBox({ loadData }) {
+  return (
+    <div className="source-box">
+      {
+        instances.map((e, index) => {
+            const link = `/${e.name}`;
+            const className = `source-instance-${index}`;
+            return (
+                <button type="button" className={className} key={e.id} onClick={() => loadData(link)}>
+                    <h2>
+                        {headers[index]}
+                    </h2>
+                    <div className="bg-image-blur" />
+                    <img alt="" />
+                </button>
+                );
+            })
+      }
+    </div>
+  );
+}
+
+SourcesBox.propTypes = {
+  loadData: PropTypes.func.isRequired,
+};
